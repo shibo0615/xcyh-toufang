b7b0e720f69fc15b9c673b839f00ca75597fccae
shibobo <shibo0615@126.com>
2023-06-21 13:53:28 +0800

    初始化

--- src/main/java/com/xcyh/xcyhtoufang/utils/TestFBJavaSDK.java
+++ src/main/java/com/xcyh/xcyhtoufang/utils/TestFBJavaSDK.java
@@ -9,7 +9,7 @@ import com.facebook.ads.sdk.Campaign;
 public class TestFBJavaSDK {
 
     public static final APIContext context = new APIContext(
-            "EAARLnLxSsWMBALZAfhSED9ohqGNEw7cpTxUzPMnGBUDljZCoKBPFXdUkVFguUUGouSTRrbyVs7evDNB6ZCqf9iJO3qyTZCVKMldC4LKF5ZBKsNUvCqRrmCIxDLt8ZCJfAdYLmExraAiX6PE6c9CdE9kzU3OCRwlvPQx47ApucuSuZB5UZB0f8mun",
+            "1209036453359971|G5gZ6uAEDJWvcAyCIGuGqLMpjkM",
             "31303a5a92a1c74cb867bfffb9f8a737d64bd12607d618c0d5dd9224ec0dfe24",
             "1209036453359971"
     );
