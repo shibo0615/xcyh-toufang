b588f8858c886d0286b5a9ff6982e6005c432aba
shibobo <shibo0615@126.com>
2023-06-21 14:49:20 +0800

    初始化

--- src/main/java/com/xcyh/xcyhtoufang/utils/TestFBJavaSDK.java
+++ src/main/java/com/xcyh/xcyhtoufang/utils/TestFBJavaSDK.java
@@ -15,7 +15,7 @@ public class TestFBJavaSDK {
     );
     public void test()
     {
-        AdAccount account = new AdAccount("act_{578942610814600}", context);
+        AdAccount account = new AdAccount("act_578942610814600", context);
         try {
             APINodeList<Campaign> campaigns = account.getCampaigns().requestAllFields().execute();
             for(Campaign campaign : campaigns) {
