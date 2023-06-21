af4c0407473dc352df76e49db34f1ac5b092f70d
shibobo <shibo0615@126.com>
2023-06-21 12:03:05 +0800

    初始化

--- src/main/java/com/xcyh/xcyhtoufang/XcyhToufangApplication.java
+++ src/main/java/com/xcyh/xcyhtoufang/XcyhToufangApplication.java
@@ -1,6 +1,5 @@
 package com.xcyh.xcyhtoufang;
 
-import com.xcyh.xcyhtoufang.utils.TestFBJavaSDK;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +8,6 @@ public class XcyhToufangApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(XcyhToufangApplication.class, args);
-
-        TestFBJavaSDK testFBJavaSDK = new TestFBJavaSDK();
-        testFBJavaSDK.test();
-
     }
 
 }
--- src/main/java/com/xcyh/xcyhtoufang/controller/TestController.java
+++ src/main/java/com/xcyh/xcyhtoufang/controller/TestController.java
@@ -3,6 +3,7 @@ package com.xcyh.xcyhtoufang.controller;
 import com.alibaba.fastjson.JSON;
 import com.xcyh.xcyhtoufang.pojo.Test001;
 import com.xcyh.xcyhtoufang.service.TestService001;
+import com.xcyh.xcyhtoufang.utils.TestFBJavaSDK;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +41,10 @@ public class TestController {
             id = Integer.parseInt(idStr);
         }
         Test001 test001 = testService001.getTest001(id);
+
+        TestFBJavaSDK testFBJavaSDK = new TestFBJavaSDK();
+        testFBJavaSDK.test();
+
         System.out.println("action调用；；；；；；；；；；id = " + idStr);
         System.out.println("返回结果： test001 = " + JSON.toJSONString(test001));
 
